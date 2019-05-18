aa1313d9425f96db41c263bd24d8d051842333a2
Abhishek6264 <abhishek56940@gmail.com>
2019-05-18 15:52:27 +0530

    book update/delete
    delete funcationality added

---
+++ Lib_Mang/www/js/services.js
@@ -0,0 +1,6 @@
+angular.module('starter.services', [])
+
+// .factory('Books', function() {
+//   var book_detials = JSON.parse(localStorage.getItem('book_list'));
+//  console.log(book_detials);
+// });
